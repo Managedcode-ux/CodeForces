23d5675538f20963b09cea48e93329e9564fdf8b
Mayank <mayank.mehra.dev@gmail.com>
2021-07-21 11:38:20 +0530

    Solved

---
+++ calculating_function.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main()
+{
+   long long int n,sum=0;
+    cin>>n;
+    if(n%2==0)
+    {
+        cout<<n/2;
+    }
+   else 
+   {
+       cout<<-((n+1)/2);
+   }
+
+}
