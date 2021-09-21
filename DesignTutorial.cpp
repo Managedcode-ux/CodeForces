274ba254e83fefaf4788021e3fba54709e8d92be
Mayank <mayank.mehra.dev@gmail.com>
2021-09-21 13:07:34 +0530

    Solved

---
+++ DesignTutorial.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    long long int n;
+    cin>>n;
+    if(n%2 ==0){
+        cout<<n-4<<" 4";
+    }
+    else{
+        cout<<n-9<<" 9";
+    }
+
+}
+
